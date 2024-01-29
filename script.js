044df53280fae6f7ecc420afe499c4813ff24db5
Gurumoorthy M <gurunaveen22@gmail.com>
2024-01-29 12:21:57 +0530

    Work on humberger menu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
-    menu.className.toggle('open');
-    icon.className.toggle('open');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
 }
